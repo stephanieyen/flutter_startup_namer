e656dece81daa4f42c0ca5579be991285b46a083
Stephanie Yen <stephanieyen@Stephanies-Air.lan>
2022-06-29 13:42:46 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget { // app is a stateless widget
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center( // body property that holds (centered) widget subtree for home screen
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+// Creates instance of a State class
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+// Generic State class specialized for use with RandomWords
+// Maintains state for the RandomWords widget by saving list of generated word pairs
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
