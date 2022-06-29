aba9b57b70ebb6d9bea886670000fabe35983965
Stephanie Yen <stephanieyen@Stephanies-Air.lan>
2022-06-29 16:36:07 -0400

    created a Flutter app with Dart code with the following features: - third-party library - stateful widget - lazily loaded, infinite scolling list

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget { // app is a stateless widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center( // body property that holds (centered) widget subtree for home screen
           child: RandomWords(),
@@ -37,7 +37,43 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    final _suggestions = <WordPair>[]; // list of suggested word pairs
+    final _biggerFont = const TextStyle(fontSize: 18); // make font size larger
+
+    // final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+
+    // ListView widget to display the suggested word pairs
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+
+      // Builder property (itemBuilder) 
+      // Called once per suggested word pair to place it into ListTile row
+      // Why:        Allows suggested list to continue growing as the user scrolls
+      // What:       Factory builder + callback function = anonymous function
+      // Parameters: BuildContext (context)
+      //             row iterator (i) which increments with each call 
+      itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider(); // visually separate rows
+
+        final index = i ~/ 2; // calculates actual # of pairs (minus dividers)
+        
+        // If end of list has been reached, then generate 10 more to add
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        // // Create and display a Text widget per word pair
+        // return Text(_suggestions[index].asPascalCase);
+
+        // Return each word pair as a ListTile (fixed height row w/ text + icons/widgets)
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase, 
+            style: _biggerFont, 
+          ),
+        );
+      }
+    );
   }
 }
